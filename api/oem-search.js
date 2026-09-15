875dbee15fcf74789a6c4252dd67d63bc92b5c64
MihaiAlexandruC <mihai.alexandruconstantin@yahoo.com>
2026-09-15 12:28:27 +0300

    Create oem-search.js

---
+++ api/oem-search.js
@@ -0,0 +1,18 @@
+// GET /api/oem-search?code=8F0513035N&langId=4
+// Proxy pentru: GET /api/articles-oem/search-by-article-oem-no?articleOemNo=...&langId=...
+import { fetchFromAutoPartsAPI, sendError } from "./_lib/autopartsapi.js";
+
+export default async function handler(req, res) {
+  const { code, langId = "4" } = req.query;
+  if (!code) {
+    return res.status(400).json({ error: "bad_request", message: "Lipsește parametrul code" });
+  }
+  try {
+    const data = await fetchFromAutoPartsAPI(
+      /api/articles-oem/search-by-article-oem-no?articleOemNo=${encodeURIComponent(code)}&langId=${langId}
+    );
+    return res.status(200).json(data);
+  } catch (err) {
+    return sendError(res, err);
+  }
+}
