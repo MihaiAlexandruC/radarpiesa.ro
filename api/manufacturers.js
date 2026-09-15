ab12a60e30270240d5f8294716cb803f6b0f969c
MihaiAlexandruC <mihai.alexandruconstantin@yahoo.com>
2026-09-15 12:19:31 +0300

    Add manufacturers API handler
    Implement API endpoint to fetch manufacturers by typeId.

---
+++ api/manufacturers.js
@@ -0,0 +1,14 @@
+// GET /api/manufacturers?typeId=1
+// Proxy pentru: GET /api/manufacturers/list/type-id/{typeId}
+import { fetchFromAutoPartsAPI, sendError } from "./_lib/autopartsapi.js";
+
+export default async function handler(req, res) {
+  const typeId = req.query.typeId || "1"; // 1 = mașini de pasageri
+  try {
+    const data = await fetchFromAutoPartsAPI(/api/manufacturers/list/type-id/${typeId});
+    res.setHeader("Cache-Control", "s-maxage=86400"); // brandurile se schimbă rar, cache 24h
+    return res.status(200).json(data);
+  } catch (err) {
+    return sendError(res, err);
+  }
+}
