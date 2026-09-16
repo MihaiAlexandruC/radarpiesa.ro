70cd5227848b107b5a753f63bb14ccdf0ce23872
MihaiAlexandruC <mihai.alexandruconstantin@yahoo.com>
2026-09-16 14:56:23 +0300

    Update vehicles.js

--- api/vehicles.js
+++ api/vehicles.js
@@ -1,15 +1,15 @@
-// GET /api/models?manufacturerId=16&typeId=1&langId=4&countryFilterId=63
-// Proxy pentru: GET /api/models/list/type-id/{typeId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}
+// GET /api/vehicles?modelId=5626&typeId=1&langId=4&countryFilterId=63
+// Proxy pentru: GET /api/types/type-id/{typeId}/list-vehicles-types/{modelId}/lang-id/{langId}/country-filter-id/{countryFilterId}
 import { fetchFromAutoPartsAPI, sendError } from "./_lib/autopartsapi.js";
 
 export default async function handler(req, res) {
-  const { manufacturerId, typeId = "1", langId = "4", countryFilterId = "63" } = req.query;
-  if (!manufacturerId) {
-    return res.status(400).json({ error: "bad_request", message: "Lipsește manufacturerId" });
+  const { modelId, typeId = "1", langId = "4", countryFilterId = "63" } = req.query;
+  if (!modelId) {
+    return res.status(400).json({ error: "bad_request", message: "Lipsește modelId" });
   }
   try {
     const data = await fetchFromAutoPartsAPI(
-      `/api/models/list/type-id/${typeId}/manufacturer-id/${manufacturerId}/lang-id/${langId}/country-filter-id/${countryFilterId}`
+      /api/types/type-id/${typeId}/list-vehicles-types/${modelId}/lang-id/${langId}/country-filter-id/${countryFilterId}
     );
     res.setHeader("Cache-Control", "s-maxage=86400");
     return res.status(200).json(data);
