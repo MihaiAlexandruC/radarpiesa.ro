b4de0f7037c59ef99f252aed62e431fd725e1f91
MihaiAlexandruC <mihai.alexandruconstantin@yahoo.com>
2026-09-16 10:03:08 +0300

    Update manufacturers.js

--- api/manufacturers.js
+++ api/manufacturers.js
@@ -5,7 +5,7 @@ import { fetchFromAutoPartsAPI, sendError } from "./_lib/autopartsapi.js";
 export default async function handler(req, res) {
   const typeId = req.query.typeId || "1"; // 1 = mașini de pasageri
   try {
-    const data = await fetchFromAutoPartsAPI(/api/manufacturers/list/type-id/${typeId});
+    const data = await fetchFromAutoPartsAPI(`/api/manufacturers/list/type-id/${typeId}`);
     res.setHeader("Cache-Control", "s-maxage=86400"); // brandurile se schimbă rar, cache 24h
     return res.status(200).json(data);
   } catch (err) {
